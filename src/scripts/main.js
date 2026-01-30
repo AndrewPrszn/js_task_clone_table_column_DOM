928184cc9589ef17eb9d7d7ad498b0280ea3d24b
MacBook Pro <macbookpro@Andrew.local>
2026-01-30 18:19:44 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+
+const headerRow = table.querySelector('thead tr');
+const secondTh = headerRow.children[1];
+
+headerRow.insertBefore(secondTh.cloneNode(true), headerRow.children[4]);
+
+const tbodyRows = table.querySelector('tbody').rows;
+
+for (const tr of tbodyRows) {
+  const secondTd = tr.children[1];
+
+  tr.insertBefore(secondTd.cloneNode(true), tr.children[4]);
+}
